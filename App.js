48824071511f4453d5db84bc3d02f23aea85f5db
Gottfrid Ericsson <westraccoon@yahoo.com>
2019-12-14 11:38:14 +0300

    a new function bream

--- App.js
+++ App.js
@@ -1,27 +1,38 @@
-import TelegramBot from 'node-telegram-bot-api'
+const TelegramBot =require( 'node-telegram-bot-api')
 
-const token = '1015454315:AAHX4KcCPC1A5dKLmyzL5p5hbIvMTTweaPY'
+const token = '1015454315:AAE4Pri3dlFPlnceH9DC67g7w4ge5C17fK8'
 const gottfrid_bot = new TelegramBot(token, {polling: true})
 
-
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-    // 'msg' is the received Message from Telegram
-    // 'match' is the result of executing the regexp above on the text content
-    // of the message
+gottfrid_bot.onText(/\/echo (.+)/, (msg, match) => {
 
     const chatId = msg.chat.id;
-    const resp = match[1]; // the captured "whatever"
+    const resp = match[1]; 
 
-    // send back the matched "whatever" to the chat
-    bot.sendMessage(chatId, resp);
+   gottfrid_bot.sendMessage(chatId, resp);
 });
+gottfrid_bot.onText(/\/bream(.+)/,(msg,match)=>{
+    const chatId = msg.chat.id
+        const id_person =msg.from.id
+        const personName =msg.from.username
+        const  resp = match[1]
+       // gottfrid_bot.sendMessage(id_person,`${resp}, Вам от @${personName}`)
+        const photobream ='img/bream.jpg'
+        gottfrid_bot.sendPhoto(chatId,
+                            photobream,
+                            {
+                                caption: `${resp}, Вам от @${personName}`
+                            }
 
-// Listen for any kind of message. There are different kinds of
-// messages.
-bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
+
+
+        )
+
+})
+
+
+//gottfrid_bot.on('message', (msg) => {
+  //  const chatId = msg.chat.id;
 
     // send a message to the chat acknowledging receipt of their message
-    bot.sendMessage(chatId, 'Received your message');
-});
+ //   gottfrid_bot.sendMessage(chatId, 'Received your message');
+//});
